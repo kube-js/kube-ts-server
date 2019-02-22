d6dfc2b24451e3d7eb356d7d72934eacb13c3eff
Mariocoski <mariuszrajczakowski@gmail.com>
2019-02-22 21:50:08 +0000

    changpackage.json and configs

---
+++ src/presenter/express/index.ts
@@ -0,0 +1,18 @@
+import sourceMapSupport from "source-map-support";
+sourceMapSupport.install();
+import express from "express";
+
+const app: express.Application = express();
+
+const presenterFacade = (_req: express.Request, res: express.Response) => {
+  res.json({
+    success: "ok"
+  });
+};
+
+app.use("/api/v1", presenterFacade);
+
+app.listen("3001", () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Listening on port 3001`);
+});
