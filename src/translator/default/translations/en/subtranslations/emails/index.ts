619ff2145f362d7feb0275326844722f58370d0f
kube-js <mariuszrajczakowski@pm.me>
2019-04-07 01:23:34 +0100

    fixes snapshot

--- src/translator/default/translations/en/subtranslations/emails/index.ts
+++ src/translator/default/translations/en/subtranslations/emails/index.ts
@@ -1,10 +1,10 @@
 import Emails from '../../../interfaces/Emails';
 
 const emails: Emails = {
-  resetPasswordHtml: (link: string) => `<h1>Hello,</h1><p>To reset your password click <a href="${link}" target="_blank">here</a></p><p>kube-ts-server</p>`,
+  resetPasswordHtml: (link: string) => `<h1>Hello,</h1><p>To reset your password click <a href="${link}">here</a></p><p>kube-ts-server</p>`,
   resetPasswordSubject: () => `[kube-ts-server] Reset your password`,
   resetPasswordText: (link: string) => `Hello, to reset your password click: ${link}. kube-ts-server`,
-  verifyYourEmailHtml: (link: string) => `<h1>Hello,</h1><p>To activate your account click <a href="${link}" target="_blank">here</a>.</p><p>kube-ts-server</p>`,
+  verifyYourEmailHtml: (link: string) => `<h1>Hello,</h1><p>To activate your account click <a href="${link}">here</a>.</p><p>kube-ts-server</p>`,
   verifyYourEmailSubject: () => `[kube-ts-server] Verify your email`,
   verifyYourEmailText: (link: string) => `Hello, to activate your account click: ${link}. kube-ts-server`,
 };
