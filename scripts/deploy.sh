d0cba6d368950334438abbad65d4a82b7306a40e
kube-js <mariuszrajczakowski@pm.me>
2019-05-08 00:43:57 +0100

    feat: updated deployment process

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,32 @@
-# set -e
-# HELM_DEPLOY_NAME=${APP_NAME}
+#! /bin/bash
+# exit script when any command ran here returns with non-zero exit code
+set -e
 
-# helm upgrade --kube-context ${KUBE_CONTEXT} --wait --install \
-#   --set ORG_COMPONENT_IMAGE_FULL=${ORG_COMPONENT_IMAGE_SHA} \ # echo ${ORG_DOCKER_REGISTRY}/${ORG_IMAGE_NAME
-#   --set GIT_COMMIT=${GIT_COMMIT} \ #manual
-#   --set APP_VERSION=${APP_VERSION} \ #manual
-#   --set ENVIRONMENT=${ENVIRONMENT} \ #dev
-#   --values ./k8s/charts/values.yaml \
-#   --values ./k8s/charts/values-${ENVIRONMENT}.yaml \ # dev / prod
-#   \$HELM_DEPLOY_NAME ./k8s/charts/ \
-#   --namespace=kube-ts-${ENVIRONMENT} # dev / prod
+cd ~/repo/k8s
+
+export API_VERSION="$(grep "appVersion" Chart.yaml | cut -d" " -f2)"
+export RELEASE_NAME="kube-ts-server-v${API_VERSION/./-}"
+export DEPLOYS=$(helm ls | grep $RELEASE_NAME | wc -l)
+
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+
+envsubst <./values-circleci.yml >./values-circleci.yml.out
+mv ./values-circleci.yml.out ./values-circleci.yml
+
+./kubectl config set-credentials cicd \
+--kubeconfig=/dev/null \
+--server=$KUBERNETES_SERVER \
+--certificate-authority=cert.crt \
+--token=$KUBERNETES_TOKEN \
+--embed-certs=true \
+
+echo "initialising helm..."
+helm init --client-only
+
+echo "installing/upgrading new release..."
+
+if [ ${DEPLOYS}  -eq 0 ];
+then helm install --name=${RELEASE_NAME} --kubeconfig ~/repo/k8s/cert.crt . -f values-circleci.yaml ; 
+else helm upgrade ${RELEASE_NAME} --kubeconfig ~/repo/k8s/cert.crt . -f values-circleci.yaml ; fi
+
+echo "deployment completed..."
