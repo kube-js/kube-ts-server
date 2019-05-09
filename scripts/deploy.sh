36ecae6dd73b0c096675ad1599961bee6f0c5165
Mariusz Rajczakowski <mariuszrajczakowski@pm.me>
2019-05-09 13:32:09 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,11 +16,11 @@ ls
 envsubst <~/repo/k8s/values-circleci.yaml >~/repo/k8s/values-circleci.yaml.out
 mv ~/repo/k8s/values-circleci.yaml.out ~/repo/k8s/values-circleci.yaml
 
-which kubectl
+echo "getting contexts"
 
-kubectl config get-contexts
+./kubectl config get-contexts
 
-kubectl config set-credentials cicd \
+./kubectl config set-credentials cicd \
 --kubeconfig=/dev/null \
 --server=$KUBERNETES_SERVER \
 --certificate-authority=cert.crt \
