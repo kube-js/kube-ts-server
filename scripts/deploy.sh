cdf270ba56884d64413d8655284cfe288853ae7d
kube-js <mariuszrajczakowski@pm.me>
2019-05-10 21:37:07 +0100

    fix: updated env values for logger

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,6 @@ set -e
 cd ~/repo/k8s
 
 export APP_NAME="kube-ts-server"
-export DEPLOYS=$(helm ls | grep $APP_NAME | wc -l)
-
-pwd
-ls 
 
 envsubst <~/repo/k8s/values-circleci.yaml >~/repo/k8s/values-circleci.yaml.out
 mv ~/repo/k8s/values-circleci.yaml.out ~/repo/k8s/values-circleci.yaml
@@ -24,5 +20,7 @@ helm --kubeconfig ./cicd-config.yaml list
 echo "installing/upgrading new release..."
 
 helm upgrade --install --wait --kubeconfig ./cicd-config.yaml ${APP_NAME} . -f ./values-circleci.yaml
+
+helm --kubeconfig ./cicd-config.yaml list
 
 echo "deployment completed..."
