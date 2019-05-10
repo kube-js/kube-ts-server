3c2e70be53962f75c74787875e3757efb69753fd
kube-js <mariuszrajczakowski@pm.me>
2019-05-10 18:23:42 +0100

    fixing deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,30 +8,25 @@ export API_VERSION="$(grep "appVersion" Chart.yaml | cut -d" " -f2)"
 export RELEASE_NAME="kube-ts-server-v${API_VERSION/./-}"
 export DEPLOYS=$(helm ls | grep $RELEASE_NAME | wc -l)
 
-echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
-
 pwd
 ls 
 
 envsubst <~/repo/k8s/values-circleci.yaml >~/repo/k8s/values-circleci.yaml.out
 mv ~/repo/k8s/values-circleci.yaml.out ~/repo/k8s/values-circleci.yaml
 
-which kubectl
+envsubst <~/repo/k8s/cicd-config.yaml >~/repo/k8s/cicd-config.yaml.out
+mv ~/repo/k8s/cicd-config.yaml.out ~/repo/k8s/cicd-config.yaml
 
-kubectl config set-credentials cicd \
---kubeconfig=/dev/null \
---server=$KUBERNETES_SERVER \
---certificate-authority=cert.crt \
---token=$KUBERNETES_TOKEN \
---embed-certs=true \
 
 echo "initialising helm..."
-helm init --service-account tiller
+helm init --client-only
+
+helm --kubeconfig ./config.yaml list
 
 echo "installing/upgrading new release..."
 
 if [ ${DEPLOYS}  -eq 0 ];
-then helm install --name=${RELEASE_NAME} . -f values-circleci.yaml ; 
-else helm upgrade ${RELEASE_NAME} . -f values-circleci.yaml ; fi
+then helm install --kubeconfig ./config.yaml --name=${RELEASE_NAME} . -f values-circleci.yaml ; 
+else helm upgrade --kubeconfig ./config.yaml ${RELEASE_NAME} . -f values-circleci.yaml ; fi
 
 echo "deployment completed..."
