31db7b419723f4ca022f031db0bb5a52428f4326
kube-js <mariuszrajczakowski@pm.me>
2019-08-25 23:36:50 +0100

    updated js-items dependencies

--- src/utils/errors/http/ConflictError.ts
+++ src/utils/errors/http/ConflictError.ts
@@ -2,9 +2,9 @@ import { BaseError } from 'make-error';
 
 export default class ConflictError extends BaseError {
   public readonly itemName: string;
-  public readonly itemId: string;
+  public readonly itemId?: string;
 
-  constructor(itemName: string, itemId: string) {
+  constructor(itemName: string, itemId?: string) {
     super();
     this.itemName = itemName;
     this.itemId = itemId;
