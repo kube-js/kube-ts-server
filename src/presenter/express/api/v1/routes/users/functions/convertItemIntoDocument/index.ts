ef6810ba42c289b154d0ebf654c7ce05b1fbe2e2
kube-js <mariuszrajczakowski@pm.me>
2019-09-04 22:29:32 +0100

    fix: fixed user token bug for resendVerifyToken

--- src/presenter/express/api/v1/routes/users/functions/convertItemIntoDocument/index.ts
+++ src/presenter/express/api/v1/routes/users/functions/convertItemIntoDocument/index.ts
@@ -1,13 +1,13 @@
 import ItemIntoDocument from '@js-items/express/dist/types/ItemIntoDocument';
 import { toSnake } from 'convert-keys';
 import User from '../../../../../../../../types/items/User';
-import getVisibleUserProperties from '../../../../../../../../utils/helpers/model/getVisibleUserProperties';
+import getVisiblePublicUserProperties from '../../../../../../../../utils/helpers/model/getVisiblePublicUserProperties';
 import Config from '../../../../../../presenterFactory/Config';
 
 const convertItemIntoDocument = (_config: Config): ItemIntoDocument<User> => ({
   item,
 }) => {
-  const user = getVisibleUserProperties(item);
+  const user = getVisiblePublicUserProperties(item);
 
   return toSnake(user);
 };
